639311f82c4def54c9ba66a2fce0b1a7f8a55ba2
Гаджиев Самир Надирович <gadziev.s.n@edu.mirea.ru>
2025-11-29 01:37:51 +0300

    com3

---
+++ lib/providers/shopping_list_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shopping_list_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$shoppingListHash() => r'6d607a55c3c289996ea1c0a9b37bb36b57741a68';
+
+/// See also [ShoppingList].
+@ProviderFor(ShoppingList)
+final shoppingListProvider =
+    AutoDisposeNotifierProvider<ShoppingList, List<ShoppingItem>>.internal(
+  ShoppingList.new,
+  name: r'shoppingListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$shoppingListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ShoppingList = AutoDisposeNotifier<List<ShoppingItem>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
